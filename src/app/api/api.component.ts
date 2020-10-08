95afec393a9ac02e16103d06ee6218daf904980f
dimakoptev <dimakoptev87@gmail.com>
2020-10-08 20:38:55 +0300

    08,10

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -8,29 +8,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiComponent implements OnInit {
 
-  apiUrl = 'https://et.wikipedia.org/api/rest_v1/page/related/';
-  searchApi = 'https://en.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json'
+  apiUrl = 'https://et.wikipedia.org/api/rest_v1/feed/featured/';
+  // searchApi = 'https://en.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json'
   searchResult: any;
   searchImage : string;
+  searchList : any;
 
   @ViewChild('search') searchBox: ElementRef<HTMLInputElement>;
+  @ViewChild('search1') searchBox1: ElementRef<HTMLInputElement>;
+  @ViewChild('search2') searchBox2: ElementRef<HTMLInputElement>;
+
 
   constructor(public http: HttpClient) {  }
 
   ngOnInit() { }
 
   startSearch () {
-    const searchTerm = this.searchBox.nativeElement.value;
+    const searchTerm = this.searchBox.nativeElement.value +"/"+ this.searchBox1.nativeElement.value +"/"+ this.searchBox2.nativeElement.value;
 
-    this.http.get( this.apiUrl + searchTerm, {
-      headers: {
-        "Accept-Language": "et_ee"
-      }
-    } ).subscribe((res)=> {
+
+    this.http.get( this.apiUrl + searchTerm).subscribe((res)=> {
       console.log(res);
       this.searchResult = res;
+      this.searchList = this.searchResult.mostread;
       this.searchImage = this.searchResult.thumbnail ?
       this.searchResult.thumbnail.source: undefined;
+      console.log(this.searchList)
     })
   }
   getImageUrl(page){
